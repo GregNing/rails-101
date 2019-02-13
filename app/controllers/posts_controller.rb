5e702c039299465d9702f8f45fee3c9d1489c449
Greg <GregJieNing@gmail.com>
2019-02-13 18:57:00 +0800

    Add RESTFul routing for Post and relation to group and add Post CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,49 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: %i[edit update destroy]
+  before_action :find_group
 
+  def new
+    @post = @group.posts.build
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @post = @group.posts.new(post_params)
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to group_path(@group)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def find_post
+    @post = @group.posts.find(params[:id])
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
 end
