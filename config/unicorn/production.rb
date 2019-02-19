2dab8c4a561702c67832692b199a847d195d2d57
Greg <GregJieNing@gmail.com>
2019-02-19 16:30:33 +0800

    Set Unicorn Preload

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -5,3 +5,19 @@ pid '/home/apps/rails-101/shared/tmp/pids/unicorn.pid'
 
 stderr_path 'log/unicorn.error.log'
 stdout_path 'log/unicorn.log'
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+before_fork do
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.establish_connection
+end
